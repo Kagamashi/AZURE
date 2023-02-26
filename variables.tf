a8600dbf47c1ffe071e44d19bc89793a3e77caa0
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2023-02-26 21:55:11 +0100

    Added files for storing Variables and Outputs

---
+++ variables.tf
@@ -0,0 +1,16 @@
+### UNIVERSAL ###
+
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "tenant_id" {
+  type    = string
+  default = "c7c91135-2f63-4ba4-ab4f-3c7e7ffc5d92"
+}
+
+variable "identity_type" {
+  type    = string
+  default = "UserAssigned"
+}
