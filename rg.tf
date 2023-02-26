81ea19a5cc6608549f69dc326e082e4485a09cec
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2023-02-26 21:54:24 +0100

    [RG] Added commented RG for AKS purposes

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "RG_MB_u767835"
   location = var.location
 }
+
+# resource "azurerm_resource_group" "aks" {
+#   name     = "RG_MB_u767835_AKS"
+#   location = var.location
+# }
